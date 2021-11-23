4a665b3c619eceed96f2f17e9b7665721bfa03b6
TonyBrb <anthonybarbagallo95@gmail.com>
2021-11-23 16:50:52 +0100

    creazione componente Hero

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div>
     <Title />
+    <Hero />
   </div>
 </template>
 
 <script>
 
 import Title from "./components/Title.vue"
+import Hero from "./components/Hero.vue"
 
 export default {
   name: 'App',
   components: {
-    Title
+    Title,
+    Hero
   }
 }
 </script>
 
 <style lang="scss">
+    body{
+      background-color: black;
+    }
     h1{
+      text-align: center;
+      font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
       color: blue;
+    }
+
+    .jumbotron{
+      width:60%;
+      margin: 0 auto;
     }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="jumbotron">
+    <img src="../assets/valentino-rossi.png" alt="">
+    <p>MotoGP legend Valentino Rossi greets his fans for the last time. 46 always in our hearts!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+
+}
+</script>
+
+<style scoped>
+  .jumbotron img{
+    width: 100%;
+  }
+  p{
+    font-size: 20px;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    color: yellow;
+
+  }
+</style>
