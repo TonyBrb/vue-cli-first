47032b7a7dc97f6c4f8fc1640a0d68d2e7f18280
TonyBrb <anthonybarbagallo95@gmail.com>
2021-11-23 16:20:28 +0100

    Creazione componente title e modifica in css del color

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-
+    <Title />
   </div>
 </template>
 
 <script>
 
+import Title from "./components/Title.vue"
+
 export default {
   name: 'App',
   components: {
-    
+    Title
   }
 }
 </script>
 
 <style lang="scss">
-
+    h1{
+      color: blue;
+    }
 </style>
